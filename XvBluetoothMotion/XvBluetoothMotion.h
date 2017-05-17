0dee04d35e67a9e1787734cb012158bb31e9a29f
jasonjsnell <snell.jason@gmail>
2017-05-17 10:12:57 -0500

    Initial Commit

---
+++ XvBluetoothMotion/XvBluetoothMotion.h
@@ -0,0 +1,19 @@
+//
+//  XvBluetoothMotion.h
+//  XvBluetoothMotion
+//
+//  Created by Jason Snell on 5/17/17.
+//  Copyright © 2017 Jason J. Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvBluetoothMotion.
+FOUNDATION_EXPORT double XvBluetoothMotionVersionNumber;
+
+//! Project version string for XvBluetoothMotion.
+FOUNDATION_EXPORT const unsigned char XvBluetoothMotionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvBluetoothMotion/PublicHeader.h>
+
+
